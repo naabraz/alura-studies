b628356f80c7debefee92d72e393e10cb8b82663
Natalia Braz <naabraz@gmail.com>
2023-03-20 09:06:15 -0300

    chore: add style to button

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 class Botao extends React.Component {
   render() {
+    const backgroundColor = 'red'
     return (
-      <button>
+      <button style={{
+        backgroundColor
+      }}>
         Botão
       </button>
     )

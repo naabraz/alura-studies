20ec2bde0ab3b107229739a32ae3b842b5f8fbe5
Natalia Braz <naabraz@gmail.com>
2023-03-21 09:54:23 -0300

    feat: add state

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-  const tarefas = [{
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'React',
     tempo: '02:00:00'
   }, {
@@ -11,11 +12,13 @@ function Lista() {
   }, {
     tarefa: 'Typescript',
     tempo: '03:00:00'
-  }]
+  }]);
 
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do Dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00"}])
+      }}> Estudos do Dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item

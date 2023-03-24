bc39968fd7b82ce8cd1e30c93d5a2e0870f3da27
Natalia Braz <naabraz@gmail.com>
2023-03-24 09:52:00 -0300

    feat: add regressive function

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -3,13 +3,18 @@ import style from './Botao.module.scss';
 
 class Botao extends React.Component<{
   children: ReactNode,
-  type?: "button" | "submit" | "reset" | undefined
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void
 }> {
   render() {
-    const { type = "button" } = this.props;
+    const { type = "button", onClick } = this.props;
 
     return (
-      <button type={type} className={style.botao}>
+      <button
+        onClick={onClick}
+        type={type}
+        className={style.botao}
+      >
         {this.props.children}
       </button>
     )

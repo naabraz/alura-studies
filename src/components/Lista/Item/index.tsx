3262efb90f5ac9159b3f9d6bcbe6a4f363a8bc5f
Natalia Braz <naabraz@gmail.com>
2023-03-27 09:12:44 -0300

    chore: add icon to completed task

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -16,8 +16,11 @@ export default function Item({
 
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-      onClick={() => selecionaTarefa({
+      className={`
+        ${style.item} ${selecionado ? style.itemSelecionado : ''}
+        ${completado ? style.itemCompletado : ''}
+      `}
+      onClick={() => !completado && selecionaTarefa({
         tarefa,
         tempo,
         selecionado,
@@ -26,6 +29,12 @@ export default function Item({
       })}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado &&
+        <span
+          className={style.concluido}
+          aria-label="Tarefa Completada"
+        >
+        </span>}
     </li>
   )
 }

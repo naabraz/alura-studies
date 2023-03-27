754d76e6efda4908f9f31ac40cfa0567320e1940
Natalia Braz <naabraz@gmail.com>
2023-03-27 09:24:36 -0300

    chore: refactor class component to function component

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,24 +1,22 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{
-  children: ReactNode,
+interface Props {
+  children?: ReactNode,
   type?: "button" | "submit" | "reset" | undefined,
   onClick?: () => void
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
+}
 
-    return (
-      <button
-        onClick={onClick}
-        type={type}
-        className={style.botao}
-      >
-        {this.props.children}
-      </button>
-    )
-  }
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={style.botao}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Botao;
